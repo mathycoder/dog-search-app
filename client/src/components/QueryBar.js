4db6571ecbe6725dabf4d08d0b47266901c86b20
Adam Sarli <adam.sarli@gmail.com>
2020-05-20 19:25:26 -0400

    selecting a breed brings up a sub-breed menu

--- client/src/components/QueryBar.js
+++ client/src/components/QueryBar.js
@@ -2,23 +2,63 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { searchBreeds, fetchAllBreeds } from '../actions/searchActions.js'
 
-const QueryBar = ({ fetchAllBreeds, allBreeds }) => {
-  const [query, setQuery] = useState('')
+const QueryBar = ({ fetchAllBreeds, allBreeds, subBreedObject }) => {
+  const [selectedBreed, setSelectedBreed] = useState(null)
+  const [subBreedList, setSubBreedList] = useState([])
+  const [selectedSubBreed, setSelectedSubBreed] = useState(null)
 
   useEffect(() => {
     fetchAllBreeds()
   }, [])
 
+  useEffect(() => {
+    if (allBreeds.length > 0) {
+      setSelectedBreed(allBreeds[0])
+    }
+  }, [allBreeds])
+
+  useEffect(() => {
+    if (selectedBreed){
+      const list = subBreedObject[selectedBreed]
+      setSubBreedList(list)
+      if (list.length === 0) setSelectedSubBreed(null)
+    }
+  }, [selectedBreed])
+
+  useEffect(() => {
+    subBreedList.length > 0
+      ? setSelectedSubBreed(subBreedList[0])
+      : setSelectedSubBreed(null)
+  }, [subBreedList])
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    searchBreeds(query)
+    console.log("Breed:", selectedBreed)
+    console.log("Sub-Breed:", selectedSubBreed)
+    //const query =
+    // searchBreeds(query)
   }
 
   const renderBreedDropdown = () => {
     return (
-      <select>
-        {allBreeds.map(breed => (
-          <option value={breed}>{breed}</option>
+      <select onChange={e => setSelectedBreed(e.target.value)} >
+        {allBreeds.map((breed, index) => (
+          <option
+            key={`breed-${index}`}
+            value={breed}
+          >
+            {breed}
+          </option>
+        ))}
+      </select>
+    )
+  }
+
+  const renderSubBreedDropdown = () => {
+    return (
+      <select onChange={e => setSelectedSubBreed(e.target.value)}>
+        {subBreedList.map((subBreed, index) => (
+          <option key={`subBreed-${index}`} value={subBreed}>{subBreed}</option>
         ))}
       </select>
     )
@@ -28,11 +68,7 @@ const QueryBar = ({ fetchAllBreeds, allBreeds }) => {
     <div>
       <form onSubmit={handleSubmit}>
         {renderBreedDropdown()}
-        <input
-          type='text'
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+        {subBreedList.length > 0 ? renderSubBreedDropdown() : null}
         <input
           type="submit"
           value="Search Breeds"
@@ -44,7 +80,8 @@ const QueryBar = ({ fetchAllBreeds, allBreeds }) => {
 
 const mapStateToProps = state => {
   return {
-    allBreeds: state.search.allBreeds
+    allBreeds: state.search.allBreeds,
+    subBreedObject: state.search.allBreedsSubBreeds
   }
 }
 
