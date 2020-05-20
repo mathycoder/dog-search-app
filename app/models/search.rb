ae46adbcbcf15b4c0ae62e0a708c2d7f49e90354
Adam Sarli <adam.sarli@gmail.com>
2020-05-20 18:29:16 -0400

    added ability to search for sub-breed

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,7 @@
 class Search < ApplicationRecord
   def self.fetchApiData(query)
-    resp = Faraday.get("https://dog.ceo/api/breed/#{query}/images/random/9") do |req|
+    breed = query.split("-")[0]
+    resp = Faraday.get("https://dog.ceo/api/breed/#{breed}/images/random/9") do |req|
     end
     if resp.status == 404
       "Unknown Breed"
